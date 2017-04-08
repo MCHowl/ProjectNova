a4258a8bc81440bcf175d0abeb5dfa42b70631d5
angelw22 <angelwong9510@gmail.com>
2017-04-08 14:56:49 +0800

    Added a start scene with only start buttons
    Attached a script to the button, not sure how the hover would be done.
    Requires also the font for the game name.

---
+++ Assets/Scripts/HoverOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverOver : MonoBehaviour {
+public	Button button;
+	public Image buttonimg;
+public Image darkbutton;
+	public Image lightbutton;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver()
+	{
+		transform.GetComponent<Image>();
+
+	}
+
+	void OnMouseExit()
+	{
+	}
+}
