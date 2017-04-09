02274f60f499fac136236028c3fe15fc0496b378
MCHowl <_mattc@hotmail.com>
2017-04-10 05:40:09 +0800

    Added animated keyboards for tutorial

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+
+	private float destroyTime;
+	public float persistanceDuration;
+
+	// Use this for initialization
+	void Start () {
+		destroyTime = Time.time + persistanceDuration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > destroyTime) {
+			Destroy (this.gameObject);
+		}
+	}
+}
